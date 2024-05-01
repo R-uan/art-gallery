1871b559439fd1a1c0c190da4d1d80e383b6c012
Ruan <ruan.oliveiralopes@hotmail.com>
2024-05-01 08:49:43 -0300

    footer

---
+++ src/app/(Components)/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+import s from "./Footer.module.scss";
+export default function Footer() {
+	return (
+		<footer className={s.footer}>
+			<div>
+				<span className="font-bebas text-[2rem]">digital footprint</span>
+			</div>
+		</footer>
+	);
+}
