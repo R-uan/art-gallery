4a92bfcaf4f762c82324cf057c67d4dfb35a4a3a
Ruan <ruan.oliveiralopes@hotmail.com>
2024-05-07 15:16:17 -0300

    exposition component

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,104 +1,17 @@
-import Image from "next/image";
+import ArtworkFilter from "./_components/Filter/ArtworkFilter";
 import Header from "../(Components)/Header/Header";
 import s from "./Gallery.module.scss";
+import Footer from "../(Components)/Footer/Footer";
+import Exposition from "./_components/Exposition/Exposition";
 export default function Gallery() {
 	return (
 		<body>
 			<Header transparent={false} />
 			<main className={s.gallery}>
-				<div className={s.content_box}>
-					<div className={s.img_box}>
-						<img alt="" src={"/assets/gallery-test/@gallery-test[1].jpg"} />
-					</div>
-					<div className={s.img_box}>
-						<img alt="" src={"/assets/gallery-test/@gallery-test[2].jpg"} />
-					</div>
-					<div className={s.img_box}>
-						<img alt="" src={"/assets/gallery-test/@gallery-test[3].jpg"} />
-					</div>
-					<div className={s.img_box}>
-						<img alt="" src={"/assets/gallery-test/@gallery-test[4].jpg"} />
-					</div>
-					<div className={s.img_box}>
-						<img alt="" src={"/assets/gallery-test/@gallery-test[5].jpg"} />
-					</div>
-					<div className={s.img_box}>
-						<img alt="" src={"/assets/gallery-test/@gallery-test[6].jpg"} />
-					</div>
-					<div className={s.img_box}>
-						<img alt="" src={"/assets/gallery-test/@gallery-test[7].jpg"} />
-					</div>
-					<div className={s.img_box}>
-						<img alt="" src={"/assets/gallery-test/@gallery-test[8].jpg"} />
-					</div>
-					<div className={s.img_box}>
-						<img alt="" src={"/assets/gallery-test/@gallery-test[9].jpg"} />
-					</div>
-					<div className={s.img_box}>
-						<img alt="" src={"/assets/gallery-test/@gallery-test[10].jpg"} />
-					</div>
-					<div className={s.img_box}>
-						<img alt="" src={"/assets/gallery-test/@gallery-test[11].jpg"} />
-					</div>
-					<div className={s.img_box}>
-						<img alt="" src={"/assets/gallery-test/@gallery-test[12].jpg"} />
-					</div>
-					<div className={s.img_box}>
-						<img alt="" src={"/assets/gallery-test/@gallery-test[13].jpg"} />
-					</div>
-					<div className={s.img_box}>
-						<img alt="" src={"/assets/gallery-test/@gallery-test[14].jpg"} />
-					</div>
-					<div className={s.img_box}>
-						<img alt="" src={"/assets/gallery-test/@gallery-test[15].jpg"} />
-					</div>
-					<div className={s.img_box}>
-						<img alt="" src={"/assets/gallery-test/@gallery-test[16].jpg"} />
-					</div>
-					<div className={s.img_box}>
-						<img alt="" src={"/assets/gallery-test/@gallery-test[17].jpg"} />
-					</div>
-					<div className={s.img_box}>
-						<img alt="" src={"/assets/gallery-test/@gallery-test[18].jpg"} />
-					</div>
-					<div className={s.img_box}>
-						<img alt="" src={"/assets/gallery-test/@gallery-test[19].jpg"} />
-					</div>
-					<div className={s.img_box}>
-						<img alt="" src={"/assets/gallery-test/@gallery-test[20].jpg"} />
-					</div>
-					<div className={s.img_box}>
-						<img alt="" src={"/assets/gallery-test/@gallery-test[21].jpg"} />
-					</div>
-					<div className={s.img_box}>
-						<img alt="" src={"/assets/gallery-test/@gallery-test[22].jpg"} />
-					</div>
-					<div className={s.img_box}>
-						<img alt="" src={"/assets/gallery-test/@gallery-test[23].jpg"} />
-					</div>
-					<div className={s.img_box}>
-						<img alt="" src={"/assets/gallery-test/@gallery-test[24].jpg"} />
-					</div>
-					<div className={s.img_box}>
-						<img alt="" src={"/assets/gallery-test/@gallery-test[25].jpg"} />
-					</div>
-					<div className={s.img_box}>
-						<img alt="" src={"/assets/gallery-test/@gallery-test[26].jpg"} />
-					</div>
-					<div className={s.img_box}>
-						<img alt="" src={"/assets/gallery-test/@gallery-test[27].jpg"} />
-					</div>
-					<div className={s.img_box}>
-						<img alt="" src={"/assets/gallery-test/@gallery-test[28].jpg"} />
-					</div>
-					<div className={s.img_box}>
-						<img alt="" src={"/assets/gallery-test/@gallery-test[29].jpg"} />
-					</div>
-					<div className={s.img_box}>
-						<img alt="" src={"/assets/gallery-test/@gallery-test[30].jpg"} />
-					</div>
-				</div>
+				<ArtworkFilter />
+				<Exposition />
 			</main>
+			<Footer />
 		</body>
 	);
 }
